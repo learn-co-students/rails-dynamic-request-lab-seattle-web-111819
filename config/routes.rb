bbdc029f0793efc74a471d457f2aa0b61f4ffee6
4myc <missch3n@gmail.com>
2019-12-13 14:24:47 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: :index
 
-  # resouces :students/:id, only: :show
+  # resources :students, only: [:index, :show]
   get 'students/:id', to: 'students#show'
 end
